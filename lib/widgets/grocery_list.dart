47770ae963a5e37b56e7de3178436db53ff7471c
mjr2595 <michaeljohnraymond@gmail.com>
2023-12-31 11:00:22 -0600

    Refactor adding item to remove extra fetch

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -52,13 +52,17 @@ class _GroceryListState extends State<GroceryList> {
   }
 
   void _addItem() async {
-    await Navigator.of(context).push<GroceryItem>(
+    final newItem = await Navigator.of(context).push<GroceryItem>(
       MaterialPageRoute(
         builder: (ctx) => const NewItem(),
       ),
     );
 
-    _loadItems();
+    if (newItem != null) {
+      setState(() {
+        _groceryItems.add(newItem);
+      });
+    }
   }
 
   void _removeItem(GroceryItem itemToRemove) {
--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -41,11 +41,20 @@ class _NewItemState extends State<NewItem> {
         ),
       );
 
+      final Map<String, dynamic> resData = json.decode(response.body);
+
       if (!context.mounted) {
         return;
       }
 
-      Navigator.of(context).pop();
+      Navigator.of(context).pop(
+        GroceryItem(
+          id: resData['name'],
+          name: _enteredName,
+          quantity: _enteredQuantity,
+          category: _enteredCategory!,
+        ),
+      );
     }
   }
 
